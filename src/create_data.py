0265c3ad9d1779105f720e24b74c0b32044dbff3
nbaak <niklas.baak@outlook.com>
2023-01-25 11:57:17 +0100

    improved the create file and removed database

--- src/create_data.py
+++ src/create_data.py
@@ -2,22 +2,34 @@
 import pickle
 import csv
 
-geodata = []
+
+def create() -> list:
+    geodata = []
+
+    with open("IP2LOCATION-LITE-DB1.CSV", 'r') as fp:
+        for start, stop, code, country in csv.reader(fp):
+            data = {'start': int(start), 'stop': int(stop), 'code': code, 'country': country}
+            geodata.append(data)
+
+    with open("geoip.bin", "wb") as fp:
+        pickle.dump(geodata, fp)
+
+    return geodata
 
 
-with open("IP2LOCATION-LITE-DB1.CSV", 'r') as fp:
-    reader = csv.reader(fp)
-    
-    for row in reader:
-        start, stop, code, country = int(row[0]), int(row[1]), row[2], row[3]
-        data = {'start': start, 'stop': stop, 'code': code, 'country': country}
-        geodata.append(data)
+def test(geodata:list):
+    print('testing...')
+    print(len(geodata) == 215134)
+    print(geodata[0]['stop'] == 16777215 and geodata[0]['code'] == '-', geodata[0])
+    print(geodata[10]['stop'] == 16843263 and geodata[10]['code'] == 'US', geodata[10])
+    print(geodata[1000]['stop'] == 37394431 and geodata[1000]['code'] == 'NL', geodata[1000])
+    print(geodata[10000]['stop'] == 390643711 and geodata[10000]['code'] == 'AU', geodata[10000])
 
-with open("geoip.bin", "wb") as fp:
-    pickle.dump(geodata, fp)
 
-print(len(geodata)==215134)
-print(geodata[0])
-print(geodata[10])
-print(geodata[1000])
-print(geodata[10000])
+def main():
+    geodata = create()
+    test(geodata)
+
+
+if __name__ == "__main__":
+    main()

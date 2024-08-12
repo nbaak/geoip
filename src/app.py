b6bf456a836a44e20bf683ae6bbc45cd92c6518b
nbaak <niklas.baak@outlook.com>
2024-08-12 16:00:40 +0200

    added a /version endpoint

--- src/app.py
+++ src/app.py
@@ -4,15 +4,31 @@ from geoip import Geoip
 import os
 import settings
 import secret_service
+from datetime import datetime
 
 geoip = Geoip(os.path.join(settings.THIS_PATH, 'geoip.bin'))
 app = Flask(__name__)
 
 
+# app:variables
+last_update = None
+
+
+@app.route("/version")
+def version():
+    app_data = {
+        "last_update": last_update,        
+        }
+    
+    return jsonify(app_data)
+
+
 @app.route('/update/<string:secret>')
 def update(secret):
+    global last_update
     if secret_service.verify(secret, settings.SECRET_FILE):
         if geoip.load_data():
+            last_update = datetime.now()
             return "Data loaded", 200
 
     return "ERROR", 404
